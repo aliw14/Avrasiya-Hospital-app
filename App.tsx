24e20d735f3ce3f68c044aa89b254323ef936007
aliw14 <aligm@code.edu.az>
2024-11-09 02:24:46 +0400

    SplashScreen created

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import Router from './src/router/Router';
+import BootSplash from 'react-native-bootsplash';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.root}>
--- android/app/src/main/java/com/avrasiya/MainActivity.kt
+++ android/app/src/main/java/com/avrasiya/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.avrasiya
 
+import android.os.Bundle // ⬅️ Required import for Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.zoontek.rnbootsplash.RNBootSplash // ⬅️ Required import for RNBootSplash
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "Avrasiya"
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "Avrasiya"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flag [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        RNBootSplash.init(this, R.style.BootTheme) // ⬅️ Initialize the splash screen
+        super.onCreate(savedInstanceState) // ⬅️ Corrected to pass savedInstanceState
+    }
 }
